ee9ef54abd41c4826a4e9817ab9b48f177d47a76
dpinart <dpinart@uxland.cat>
2021-02-22 14:18:00 +0100

    feat: time locale feature added

--- dates.go
+++ dates.go
@@ -1,6 +1,9 @@
 package mys_sap_client
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 //Converts a sap string datetime in format yyyyMMddHHmmss to a go time
 func FromSAPDateTime(date, t string) time.Time {
@@ -22,20 +25,39 @@ func FromSAPDate(date string) time.Time {
 	return parsed
 }
 
+var location *time.Location
+
+func SetTimeLocation(name string) {
+	l, err := time.LoadLocation(name)
+	if err != nil {
+		fmt.Printf("error setting SAP time location \n")
+		return
+	}
+	location = l
+
+}
+
+func localeTime(t time.Time) time.Time {
+	if location == nil {
+		return t
+	}
+	return t.In(location)
+}
+
 //Formats a time into a SAP date string in format yyyyMMdd
 func ToSAPDate(t time.Time) string {
 	const layout = "20060102"
-	return t.Format(layout)
+	return localeTime(t).Format(layout)
 }
 
 //Formats a time into a SAP time string in format HHmmss
 func ToSapTime(t time.Time) string {
 	const layout = "150405"
-	return t.Format(layout)
+	return localeTime(t).Format(layout)
 }
 
 //Formats a time into a SAP datetime string in format yyyyMMddHHmmss
 func ToSAPDateTime(t time.Time) string {
 	const layout = "20060102150405"
-	return t.Format(layout)
+	return localeTime(t).Format(layout)
 }
